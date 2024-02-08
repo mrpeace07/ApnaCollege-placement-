001b0a218f29a436d1414d640780561d595e421a
mr.peace <140835316+mrpeace07@users.noreply.github.com>
2024-02-08 16:49:56 +0530

    Add files via upload

---
+++ PATTERN/star.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    for ( int i=1;i<n;i++)
+    {
+        int j;
+        for (  j =1 ; j<=n-i ; j++)
+        {
+            cout<<" ";
+        }
+        for ( int k=1; k <=2*i-1 ; k++)
+        {
+            cout<< "*";
+        
+        }
+        cout<< endl;
+    }
+
+     for ( int i=n;i>=1;i--)
+    {
+        int j;
+        for (  j =1 ; j<=n-i ; j++)
+        {
+            cout<< " ";
+        }
+        for ( int k=1; k <=2*i-1 ; k++)
+        {
+            cout<< "*";
+        
+        }
+        cout<< endl;
+    }
+    return 0;
+}
